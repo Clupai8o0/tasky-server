dac691fd5dea4371a06bb8ca611c0d86dcad3226
Clupai8o0 <clupaio4@gmail.com>
2024-09-24 15:03:01 +1000

    init proj

---
+++ index.js
@@ -0,0 +1,25 @@
+// server.js
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Simple GET route
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+// A POST route that echoes the request body
+app.post("/echo", (req, res) => {
+	res.json({
+		message: "Received data:",
+		data: req.body,
+	});
+});
+
+// Start the server
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
