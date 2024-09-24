771adfa051a4ab89bbeda9c1cf0881c60183fc02
Clupai8o0 <clupaio4@gmail.com>
2024-09-24 19:25:28 +1000

    get and save data methods

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 // server.js
 const express = require("express");
 const app = express();
@@ -11,13 +14,23 @@ app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
-// A POST route that echoes the request body
-app.post("/echo", (req, res) => {
-	res.json({
-		message: "Received data:",
-		data: req.body,
-	});
+// getting all tasks and users
+app.get("/get-data", (req, res) => {
+	const resp = fs.readFileSync(path.join(__dirname, "data.json"), "utf8");
+	const data = JSON.parse(resp);
+	res.json(data);
 });
+
+// updating tasks and users
+app.post("/save-data", (req, res) => {
+	if (!req.body) 
+		return res.status(400).send("Request body is missing");
+	const data = {
+		...req.body
+	}
+	fs.writeFileSync(path.join(__dirname, "data.json"), JSON.stringify(data, null, 2));
+	res.send("Successfully saved data");
+})
 
 // Start the server
 app.listen(port, () => {
